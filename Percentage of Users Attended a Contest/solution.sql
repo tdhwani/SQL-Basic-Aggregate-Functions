b49bdc0fb4888ff51e4bbc87993f593fb0f1c988
tdhwani <64563255+tdhwani@users.noreply.github.com>
2023-06-07 20:27:10 -0400

    Create solution.sql

---
+++ Percentage of Users Attended a Contest/solution.sql
@@ -0,0 +1,18 @@
+select 
+  contest_id, 
+  round(
+    count(distinct user_id)* 100 /(
+      select 
+        count(distinct user_id) 
+      from 
+        Users
+    ), 
+    2
+  ) as percentage 
+from 
+  Register 
+group by 
+  contest_id 
+order by 
+  percentage desc, 
+  contest_id asc;
