a59edf4126af92a1f372f4bc7d49d80468713b97
tdhwani <64563255+tdhwani@users.noreply.github.com>
2023-06-07 20:28:20 -0400

    Create solution.sql

---
+++ Queries Quality and Percentage/solution.sql
@@ -0,0 +1,16 @@
+select 
+  query_name, 
+  round(
+    avg(rating / position), 
+    2
+  ) as quality, 
+  round(
+    100 * sum(
+      if(rating < 3, 1, 0)
+    ) / count(query_name), 
+    2
+  ) as poor_query_percentage 
+from 
+  Queries 
+group by 
+  query_name;
