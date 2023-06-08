7c2f10de41a6e8a4a230e4a03fe936f032e8aa7a
tdhwani <64563255+tdhwani@users.noreply.github.com>
2023-06-07 20:30:51 -0400

    Create solution.sql

---
+++ Immediate Food Delivery/solution.sql
@@ -0,0 +1,22 @@
+select 
+  round(
+    100 * sum(
+      if(
+        order_date = customer_pref_delivery_date, 
+        1, 0
+      )
+    )/ count(*), 
+    2
+  ) as immediate_percentage 
+from 
+  Delivery 
+where 
+  (customer_id, order_date) in (
+    select 
+      customer_id, 
+      min(order_date) 
+    from 
+      Delivery 
+    group by 
+      customer_id
+  );
